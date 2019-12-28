ee54969eb50ea241db3e45b655ecf36b06c3c448
Aleksandr <aleksandros-777@mail.ru>
2019-12-28 09:59:29 +0300

    added dynamic goods representation and navbar through bootstrap

--- store.js
+++ store.js
@@ -5,6 +5,9 @@
 };
 
 function ready() {
+    
+    loadGoods()
+
     var removeCartItemButtons = document.getElementsByClassName("cart__item-delete-button");
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         var button = removeCartItemButtons[i];
@@ -38,6 +41,113 @@ function ready() {
         img.addEventListener('click', openGoodImage);
     };
 };
+
+function loadGoods() {
+    var books = [
+        {title:'Война и мир',
+        image:'https://b1.culture.ru/c/365442.jpg',
+        description: '«Война и мир» — роман-эпопея Льва Николаевича Толстого. Историко-философская мысль автора большей частью проникает в роман не в виде рассуждений, а в гениально схваченных подробностях и цельных картинах, истинный смысл которых нетрудно понять всякому вдумчивому читателю.',
+        price: '212',
+        currency: ' ₽',
+        code: '1',
+        },
+        {title:'Над пропастью во ржи',
+        image:'https://img4.labirint.ru/rc/353e7964053487ba97fe00ca0fc4644c/220x340/books50/493796/cover.jpg?1437492343',
+        description:'Единственный роман Сэлинджера - "Над пропастью во ржи" - стал переломной вехой в истории мировой литературы.Название книги и имя главного героя Холдена Колфилда сделались кодовыми для многих поколений молодых бунтарей от битников и хиппи до представителей современных радикальных молодежных движений.',
+        price: '187',
+        currency: ' ₽',
+        code: '2',
+        },
+        {title:'Идиот',
+        image:' https://cdn1.ozone.ru/s3/multimedia-5/c650/6006264545.jpg',
+        description: 'Завораживающая история трагических страстей, связавших купца Парфена Рогожина, бывшую содержанку богатого дворянина Настасью Филипповну и "идеального человека" князя Мышкина — беспомощного идиота в мире корысти и зла, гласящая о том, что сострадание, возможно, единственный закон человеческого бытия.',
+        price: '144',
+        currency: ' ₽',
+        code: '3',
+        },
+        {title:'Преступление и наказание',
+        image:' https://www.moscowbooks.ru/image/book/495/w600/i495426.jpg',
+        description: 'Это и глубокий философский роман, и тонкая психологическая драма, и захватывающий детектив, и величественная картина мрачного города, в недрах которого герои грешат и ищут прощения, жертвуют собой и отрекаются от себя ради ближних и находят успокоение в смирении, покаянии, вере.',
+        price: '160',
+        currency: ' ₽',
+        code: '4',
+        },
+        {title:'На дне',
+        image:' https://cdn1.ozone.ru/multimedia/c650/1003559322.jpg',
+        description: 'Пьеса "На дне", несомненно, одна из вершин горьковского творчества. Основная тема пьесы "На дне" - правда и ложь. В пьесе противопоставлены старец Лука со своими тихими и вечными истинами и громогласный резонер Сатин, который, согласно марксистской критике, олицетворяет собой пробуждение пролетарского сознания.',
+        price: "96",
+        currency: ' ₽',
+        code: '5',
+        }
+        ];
+
+    const itemDiv = document.createElement('div');
+    itemDiv.className = "goods row"
+    itemSpan = document.createElement("span");
+    itemImg = document.createElement("img");
+    itemH3 = document.createElement("h3");
+    itemp = document.createElement("p");
+    itemInputField = document.createElement("input");
+    itemButtonField = document.createElement("button");
+        for (i = 0; i < books.length; i++) {
+
+            good = document.importNode(itemDiv, true);
+            good.innerHTML = "";
+
+            goodImage = document.importNode(itemImg, true);
+            goodDivPrice = document.importNode(itemDiv, true);
+            goodDivPrice.innerHTML = "";
+
+            goodSpanVal = document.importNode(itemSpan, true);
+            goodSpanCur = document.importNode(itemSpan, true);
+
+            goodDivDescription = document.importNode(itemDiv, true);
+            goodDivDescription.innerHTML = "";
+            goodDescription = document.importNode(itemp, true);
+            goodTitle = document.importNode(itemH3, true);;
+            goodCode = document.importNode(itemDiv, true);
+
+            good.className = "good col";
+
+            goodImage.setAttribute("src", books[i].image);
+            goodImage.setAttribute("alt",  books[i].title)
+            goodImage.className ="good__image";
+
+            goodActionsDiv = document.importNode(itemDiv, true);
+            goodActionsDiv.innerHTML = "";
+            itemInput = document.importNode(itemInputField, true);
+            itemButton = document.importNode(itemButtonField, true);
+
+            goodDivPrice.className = "good__price";
+            goodSpanVal.className = "good__price-val";
+            goodSpanCur.className = "good__price-currency";
+            goodSpanVal.textContent = books[i].price;
+            goodSpanCur.textContent = books[i].currency;
+            goodDivPrice.append(goodSpanVal, goodSpanCur);
+
+            goodDivDescription.className = "good__description";
+            goodTitle.className = "good__description-title";
+            goodCode.className = "good__code";
+            goodTitle.textContent = books[i].title;
+            goodDescription.textContent = books[i].description;
+            goodCode.textContent = "Артикул: " + books[i].code;
+            goodDivDescription.append(goodTitle, goodDescription, goodCode);
+
+            goodActionsDiv.className = "good__actions";
+            itemInput.className = "good__amount col-4";
+            itemInput.type = "number";
+            itemInput.value = "1";
+            itemButton.className = "good__button btn btn-primary";
+            itemButton.type = "button";
+            itemButton.textContent = "Добавить в корзину";
+            goodActionsDiv.append(itemInput, itemButton);
+
+            good.append(goodImage, goodDivPrice, goodDivDescription, goodActionsDiv, goodActionsDiv);
+            itemDiv.append(good);
+            
+        document.body.appendChild(itemDiv);
+    };
+  };
 
 function showCartFunc() {
     var modal = document.getElementById("myModal");
@@ -78,13 +188,6 @@ function quantityChanged(event) {
     updateCartTotal();
 };
 
-function quantityToAddChange(event) {
-    var amount = event.target;
-    if (isNaN(amount.value) || amount.value <= 0 || amount.value >= 1000){
-        amount.value = 1;
-    };
-};
-
 function addToCartClicked(event) {
     var button = event.target;
     var shopItem = button.parentElement.parentElement;
@@ -113,6 +216,7 @@ function addItemToCart(title, price, amount, imageSrc, code, total){
             return;
         };
     };
+
     var cartRowContents = `
         <div class="cart__item cart__column">
             <img class="cart__item-image" src="${imageSrc}">
@@ -156,4 +260,5 @@ function updateCartTotal() {
     document.getElementsByClassName('cart__total-amount')[0].innerText = totalAmount;
     document.getElementsByClassName('cart__total-price')[0].innerText = total + ' ₽';
 };
+
 })();
