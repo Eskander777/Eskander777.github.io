b660cead8c8fb2911e97ffe76053f2ecd208c8db
Aleksandr <aleksandros-777@mail.ru>
2020-01-04 09:46:31 +0300

    add separate books.json file as DB moved to python-flask server

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/customer', methods=['GET', 'POST'])
 def form():
-    data = request.get_json(force=True)
-    user = data.copy()
-    order = user.pop("cartOrder")
-    for key, value in data.items():
-        print(key, '-', value)
-    print()
-    for key, value in user.items():
-        print(key, '-', value)
+    order_data = request.get_json(force=True)
+    user = order_data.copy()
+    order_items = user.pop("cartOrder")
     return "hello"
 
+@app.route('/')
+def show_books():
+    with open('books.json', encoding='utf-8') as f:
+        books = json.load(f)
+    return render_template('index.html', booksLoad=books)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
