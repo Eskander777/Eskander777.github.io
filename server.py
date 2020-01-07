22d56e34302ecd294b85c71c03eff403050aab11
Aleksandr <aleksandros-777@mail.ru>
2020-01-07 15:01:10 +0300

    added saving to customer and order

--- server.py
+++ server.py
@@ -6,10 +6,25 @@ app = Flask(__name__)
 
 @app.route('/customer', methods=['POST'])
 def customer_form():
-    order_data = request.get_json(force=True)
-    user = order_data.copy()
-    order_items = user.pop("cartOrder")
-    return "hello"
+    if request.is_json:
+        order_data = request.get_json(force=True)
+        user = order_data.copy()
+        order_items = user.pop("cartOrder")
+
+        with open('customers.json', encoding='utf8') as c_r:
+            customers = json.load(c_r)
+        customers.append(user)
+        with open('customers.json', 'w', encoding='utf8') as c:
+            json.dump(customers, c, ensure_ascii=False)
+        with open('orders.json', encoding='utf8') as o_r:
+            orders = json.load(o_r)
+        orders.append(order_data)
+        with open('orders.json', 'w', encoding='utf8') as  o:
+            json.dump(orders, o, ensure_ascii=False)
+    else:
+        pass
+
+    return "Your order is added to 'Orders'"
 
 
 @app.route('/')
